19e1a52f0cc7b0018a8d5b105b5c39e7b2dab454
Ryan Wilkinson <charlesryanwilkinson@gmail.com>
2017-05-05 14:55:49 -0400

    create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,66 +1,52 @@
-class Api::EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
-
-  # GET /events
-  # GET /events.json
-  def index
-    @events = Event.all
-  end
-
-  # GET /events/1
-  # GET /events/1.json
-  def show
-    @event = Event.find_by(user_id: current_user.id)
-  end
+class EventsController < ApplicationController
 
   # GET /events/new
   def new
+  	@organization = Organization.find(params[:organization_id])
     @event = Event.new
+  end
+
+  # GET /events/1
+  def show
+  end
+
+  # POST /events
+  def create
+  	@event = Event.create(
+      name: event_params[:name],
+      city: event_params[:city],
+      state: event_params[:state],
+      photo_url: event_params[:photo_url],
+      date: event_params[:date],
+      deadline: event_params[:deadline],
+      estimated_attendees: event_params[:estimated_attendees],
+      venue: event_params[:venue],
+      description: event_params[:description],
+      organization_id: params[:organization_id]
+      )
+  	@organization = @event.organization
+  	redirect_to "/organizations/#{@organization.id}"
   end
 
   # GET /events/1/edit
   def edit
   end
 
-  # POST /events
-  # POST /events.json
-  def create
-    @event = Event.new(event_params)
-    end
-
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
+
   end
 
   # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
-    @event = event.find(params[:id])
     @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = event.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params
-        .permit(:name, :city, :state, :photo_url, :date, :deadline, :estimated_attendees, :venue, :organization_id, :description)
+      params.require(:event)
+        .permit(:name, :city, :state, :photo_url, :date, :deadline, :estimated_attendees, :venue, :description)
+        .merge(organization_id: params[:organization_id])
     end
 end
 
