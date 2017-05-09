7def063bcd909e43649a1e5f87c01f6aeea27dd5
Ryan Wilkinson <charlesryanwilkinson@gmail.com>
2017-05-09 12:46:04 -0400

    no pass req to edit

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -36,7 +36,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   #If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
@@ -47,7 +51,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :business_name, :business_street1, :business_street2, :business_city, :business_state, :business_phone, :password, :password_confirmation])
   end
-
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
