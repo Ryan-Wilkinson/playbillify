65536cb501dc750845002e93c99b87d305e61146
Ryan Wilkinson <charlesryanwilkinson@gmail.com>
2017-05-10 12:45:47 -0400

    can can custom rescues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
         respond_to do |format|
-          format.json { head :forbidden, content_type: 'text/html' }
-          format.html { redirect_to main_app.root_url, notice: exception.message }
-          format.js   { head :forbidden, content_type: 'text/html' }
+          if current_user.user_type == 'advertiser'
+          	format.json { head :forbidden, content_type: 'text/html' }
+          	format.html { redirect_to main_app.ads_index_path, notice: exception.message }
+          	format.js   { head :forbidden, content_type: 'text/html' }
+          else
+          	format.json { head :forbidden, content_type: 'text/html' }
+          	format.html { redirect_to main_app.organizations_path, notice: exception.message }
+          	format.js   { head :forbidden, content_type: 'text/html' }
+          end
         end
       end
       def after_sign_in_path_for(resource)
