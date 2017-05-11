818032af1e24536e580175a149564637958c615e
deviea8 <deviea8@gmail.com>
2017-05-11 01:25:19 -0400

    Attempting mailchimp

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,12 +12,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   delegate :can?, :cannot?, :to => :ability
 
+  after_create :subscribe_to_mailchimp
+
   def organization?
     user_type == "organization"
   end
 
   def advertiser?
     user_type == "advertiser"
+  end
+
+  private
+
+  def subscribe_to_mailchimp
+    SubscribeToMailchimpJob.perform_later(self)
   end
 
 end
