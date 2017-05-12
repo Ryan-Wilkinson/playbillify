2f7dd636d64578139e28b9a62356a9b596617908
Ryan Wilkinson <charlesryanwilkinson@gmail.com>
2017-05-11 21:19:27 -0400

    working

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,7 +1,7 @@
 class Ad < ApplicationRecord
 	# belongs_to :advertiser, class_name: :user, optional: true, :foreign_key => "advertiser_id"
 	belongs_to :advertiser, :class_name => "User"
-	belongs_to :event, dependent: :destroy
+	belongs_to :event
 
 	# This method associates the attribute ":image" with a file attachment
 	  has_attached_file :image, styles: { thumb: '100x100>', square: '200x200#', medium: '300x300>'}
